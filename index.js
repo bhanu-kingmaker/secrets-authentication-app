1bdaee79db05796c626bf16620fae3947b9dace6
bhanu-kingmaker <yerakalabhanupranay2004@gmail.com>
2025-09-24 14:19:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
 import env from "dotenv";
+import session from "express-session";
+import connectPgSimple from "connect-pg-simple";
 
 const app = express();
 
