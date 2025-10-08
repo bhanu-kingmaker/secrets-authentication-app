7722266e5bc0d1caa8703c0932e3331ad930d481
bhanu-kingmaker <yerakalabhanupranay2004@gmail.com>
2025-10-08 22:53:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:5500/auth/google/secrets",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     async (accessToken, refreshToken, profile, cb) => {
